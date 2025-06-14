9c0eba0839002637d2c76de491b7d3ce2bd67011
Gabriel-Caceres <gabr.caceres@duocuc.cl>
2025-06-14 17:33:32 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+opcion = 0
+
+est_civil = {"C":"Casado",
+             "S":"Soltero",
+             "V":"Viudo"}
+
+generos = {"M":"Masculino",
+           "F":"Femenino",}
+
+dicc = {"Rut": [],
+        "Nombre": [],
+        "Edad": [],
+        "Estado Civil": [],
+        "Genero": [],
+        "fecha": []}
+
+print(dicc)
+
+while opcion != 4:
+
+    if opcion == 1:
+
+    if opcion == 2:
+
+    if opcion == 3:
+
+    if opcion == 4:
